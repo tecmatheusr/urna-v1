61ca228f1f594a51144214615c683c3cf952619c
tecmateusr <tec.matheusrodrigues@gmail.com>
2022-06-01 15:40:31 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '17',
+                nome: 'Bolsonaro',
+                partido: 'PQP',
+                vice: 'Mourão',
+                fotos: [
+                    { url: 'presidente.jpg', legenda: 'Presidente' },
+                    { url: 'vice.jpg', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Lula',
+                partido: 'FDP',
+                vice: 'Geraldo Alkimin',
+                fotos: [
+                    { url: 'presidente 2.jpg', legenda: 'Presidente' },
+                    { url: 'vice2.jpg', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+            {
+                numero: '24',
+                nome: 'Doria',
+                partido: 'KKK',
+                vice: 'Marina Silva',
+                fotos: [
+                    { url: 'presida 3.jpg', legenda: 'Presidente' },
+                    { url: 'vice3.jpg', legenda: 'Vice-Presidente', small: true }
+                ]
+            }
+        ]
+    }
+
+
+
+
+
+
+
+
+
+
+
+]
